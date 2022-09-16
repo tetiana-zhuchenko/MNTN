455bc175f71d8d755e040127b4c1c634a258056d
Tanya <taniyhamail@gmail.ua>
2022-09-16 17:24:39 +0300

    Begin creating close button for navigation menu Time code - 55:00

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 const navBtn = document.querySelector('.nav-button');
+const mobileNav = document.querySelector('.mobile-nav');
+
+navBtn.addEventListener('click', function() {
+    mobileNav.classList.toggle('mobile-nav-active');
+
+});
