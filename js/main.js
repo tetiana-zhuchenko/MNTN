c4ecca3b7f51c6e4850e86d1a17f7e2ce9d332ed
Tanya <taniyhamail@gmail.ua>
2022-09-22 21:22:21 +0300

    Added "no-scroll" class to body with JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const navBtn = document.querySelector('.nav-button');
 const mobileNav = document.querySelector('.mobile-nav');
+const body = document.querySelector('body');
 
 navBtn.addEventListener('click', function() {
     mobileNav.classList.toggle('mobile-nav-active');
     navBtn.classList.toggle('nav-button-close');
+    body.classList.toggle('no-scroll');
 
 });
+
+
+window.addEventListener('click', function () {
+    console.log('click on window');
+})
