f6ab65923ea9621b5e2021fb43c70114db761380
Tanya <taniyhamail@gmail.ua>
2023-08-04 11:54:53 +0300

    feat: improved js file

--- js/main.js
+++ js/main.js
@@ -2,14 +2,30 @@ const navBtn = document.querySelector('.nav-button');
 const mobileNav = document.querySelector('.mobile-nav');
 const body = document.querySelector('body');
 
-navBtn.addEventListener('click', function() {
+
+// Button click
+navBtn.addEventListener('click', function (event) {
+    event.stopPropagation();
+    toggleMobileNav();
+
+});
+
+// Click behind navigation
+window.addEventListener('click', function () {
+    if (body.classList.contains('no-scroll')) {
+        toggleMobileNav();
+    }
+});
+
+// Stopping click inside mobile navigation menu
+mobileNav.addEventListener('click', function (event) {
+    event.stopPropagation();
+
+});
+
+function toggleMobileNav() {
     mobileNav.classList.toggle('mobile-nav-active');
     navBtn.classList.toggle('nav-button-close');
     body.classList.toggle('no-scroll');
 
-});
-
-
-window.addEventListener('click', function () {
-    console.log('click on window');
-})
+} 
